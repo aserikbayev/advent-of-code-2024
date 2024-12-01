ee5377d1bed8d10df2c642f900f8f12d7f7e59e1
Azat Serikbayev <125980868+aserikbayev@users.noreply.github.com>
2024-12-01 23:52:37 +0500

    🎨 Make it more readable

--- Solutions/Day01.fs
+++ Solutions/Day01.fs
@@ -7,65 +7,52 @@ open System.Linq
 
 type Data() =
     member x.Read() =
-        let lines = File.ReadLines(@"../../../input.txt")
+        File.ReadLines(@"../../../input.txt")
+        |> Seq.map (_.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries))
+        |> Seq.fold
+            (fun (leftList: List<int>, rightList: List<int>) arr ->
 
-        let leftList = PriorityQueue<int, int>()
-        let rightList = PriorityQueue<int, int>()
+                if arr.Length = 2 then
+                    leftList.Add(Convert.ToInt32 arr[0])
+                    rightList.Add(Convert.ToInt32 arr[1])
 
-        lines
-        |> Seq.iter (fun line ->
-            let split = line.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries)
-
-            if split.Length = 2 then
-                let l = Convert.ToInt32(split[0])
-                let r = Convert.ToInt32(split[1])
-                leftList.Enqueue(l, l)
-                rightList.Enqueue(r, r))
-
-        leftList, rightList
+                (leftList, rightList))
+            (List<int>(), List<int>())
 
 let getData () =
     let data = Data().Read()
     data
 
 let part1 =
-    let leftList, rightList = getData ()
+    let left, right = getData ()
 
-    let processQueue (leftList: PriorityQueue<int, int>, rightList: PriorityQueue<int, int>) =
-        if leftList.Count > 0 && rightList.Count > 0 then
-            let left = leftList.Dequeue()
-            let right = rightList.Dequeue()
-            Some(Math.Abs(left - right), (leftList, rightList))
-        else
-            None
+    let left_sorted = Seq.sort left
+    let right_sorted = Seq.sort right
 
-    Seq.unfold processQueue (leftList, rightList) |> Seq.sum
+    let pairs = Seq.zip left_sorted right_sorted
+
+    pairs |> Seq.map (fun (l, r) -> Math.Abs(l - r)) |> Seq.sum
+
 
 printfn $"%A{part1}"
 
 let part2 =
-    let leftQueue, rightQueue = getData ()
+    let leftList, rightList = getData ()
 
-    let toList (queue: PriorityQueue<int, int>) =
-        Enumerable.Range(0, queue.Count).Select(fun _ -> queue.Dequeue())
+    let frequencies list =
+        list
+        |> Seq.fold
+            (fun frequencyMap key ->
+                match Map.tryFind key frequencyMap with
+                | Some freq -> Map.add key (freq + 1) frequencyMap
+                | None -> Map.add key 1 frequencyMap)
+            Map.empty
 
-    let leftItems = toList leftQueue
-    let rightItems = toList rightQueue
+    let leftFreq = frequencies leftList
+    let rightFreq = frequencies rightList
 
-    // I'm sure there's a more functional way of writing this
-    let frequencyCounter list =
-        (Map.empty<int, int>, list)
-        ||> Seq.fold (fun map item -> map.Add(item, map.GetValueOrDefault(item) + 1))
-    
-    let leftFreq = frequencyCounter leftItems
-    let rightFreq = frequencyCounter rightItems
+    leftFreq.Keys
+    |> Seq.filter rightFreq.ContainsKey
+    |> Seq.sumBy (fun key -> key * leftFreq[key] * rightFreq[key])
 
-    let commonKeys = leftFreq.Keys |> Seq.filter rightFreq.ContainsKey
-
-    commonKeys |> Seq.sumBy (fun key ->
-        let leftCount = leftFreq[key]
-        let rightCount = rightFreq[key]
-        key * leftCount * rightCount
-    )
-    
 printfn $"%A{part2}"
