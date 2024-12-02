6223bb382968bcca4161ab8527479624820613c6
Azat Serikbayev <125980868+aserikbayev@users.noreply.github.com>
2024-12-03 02:45:29 +0500

    ♻️ Rewrite day 1 frequency counter without fold

--- Solutions/Day01.fs
+++ Solutions/Day01.fs
@@ -2,22 +2,17 @@
 
 open System
 open System.IO
-open System.Collections.Generic
-open System.Linq
 
 type Data() =
     member x.Read() =
-        File.ReadLines(@"../../../input.txt")
-        |> Seq.map (_.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries))
-        |> Seq.fold
-            (fun (leftList: List<int>, rightList: List<int>) arr ->
+        let lists =
+            File.ReadLines(@"../../../day01_input.txt")
+            |> Seq.map (_.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries))
 
-                if arr.Length = 2 then
-                    leftList.Add(Convert.ToInt32 arr[0])
-                    rightList.Add(Convert.ToInt32 arr[1])
+        let left, right =
+            lists |> Seq.map (fun arr -> Convert.ToInt32 arr[0]), lists |> Seq.map (fun arr -> Convert.ToInt32 arr[1])
 
-                (leftList, rightList))
-            (List<int>(), List<int>())
+        left, right
 
 let getData () =
     let data = Data().Read()
@@ -26,12 +21,14 @@ let getData () =
 let part1 =
     let left, right = getData ()
 
+    let distance (a: int, b: int) = Math.Abs(b - a)
+
     let left_sorted = Seq.sort left
     let right_sorted = Seq.sort right
 
-    let pairs = Seq.zip left_sorted right_sorted
-
-    pairs |> Seq.map (fun (l, r) -> Math.Abs(l - r)) |> Seq.sum
+    Seq.zip left_sorted right_sorted
+    |> Seq.map (fun (l, r) -> distance (l, r))
+    |> Seq.sum
 
 
 printfn $"%A{part1}"
@@ -41,12 +38,9 @@ let part2 =
 
     let frequencies list =
         list
-        |> Seq.fold
-            (fun frequencyMap key ->
-                match Map.tryFind key frequencyMap with
-                | Some freq -> Map.add key (freq + 1) frequencyMap
-                | None -> Map.add key 1 frequencyMap)
-            Map.empty
+        |> Seq.groupBy id
+        |> Seq.map (fun (key, items) -> (key, Seq.length items))
+        |> Map.ofSeq
 
     let leftFreq = frequencies leftList
     let rightFreq = frequencies rightList
