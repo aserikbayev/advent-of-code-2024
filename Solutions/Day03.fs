53e2cbf5ff5ece32adf8b6bf16fea47a5be5c041
Azat Serikbayev <125980868+aserikbayev@users.noreply.github.com>
2024-12-03 20:59:52 +0500

    ✨ Solve Day 3 (both parts)

--- Solutions/Day03.fs
+++ Solutions/Day03.fs
@@ -6,25 +6,38 @@ open System.Text.RegularExpressions
 type Data() =
     member x.Read() =
         let filename = @"../../../day03_input.txt"
-        File.ReadLines(filename) |> seq
+        File.ReadLines(filename)
+
+let getMulPairs line =
+    let pattern = Regex(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled)
+
+    pattern.Matches(line)
+    |> Seq.map (fun m -> (int m.Groups[1].Value, int m.Groups[2].Value))
 
 let part1 =
     let lines = Data().Read()
 
-    let pattern = Regex(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled)
-
-    let matches =
-        lines
-        |> Seq.map (fun line ->
-            pattern.Matches(line)
-            |> Seq.sumBy (fun m -> int m.Groups[1].Value * int m.Groups[2].Value)
-            )
-
-    Seq.sum matches
+    lines
+    |> Seq.collect getMulPairs
+    |> Seq.sumBy (fun (a, b) -> a * b)
 
 printfn $"%A{part1}"
 
 let part2 =
-    "TODO"
+    let lines = Data().Read()
+
+    let fullProgram =
+        let joined = String.concat "" lines
+        "do()" + joined + "don't()"
+
+    let getExecutableSpans (line: string) =
+        let executablePattern = Regex(@"do\(\).+?don't\(\)", RegexOptions.Compiled)
+        let executableMemory = executablePattern.Matches line
+        executableMemory |> Seq.map (_.Value)
+
+    fullProgram
+    |> getExecutableSpans
+    |> Seq.collect getMulPairs
+    |> Seq.sumBy (fun (a, b) -> a * b)
 
 printfn $"%A{part2}"
