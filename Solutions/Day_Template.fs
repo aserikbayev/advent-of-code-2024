ea78748bdc94ffb914d82b0f98860d0dc22a511d
Azat Serikbayev <125980868+aserikbayev@users.noreply.github.com>
2024-12-06 14:50:07 +0500

    🎨 Add Day template

---
+++ Solutions/Day_Template.fs
@@ -0,0 +1,18 @@
+module Year2024Day99
+
+open System.IO
+
+type Data() =
+    member x.Read() =
+        File.ReadLines(@"../../../day99_input.txt")
+
+let part1 =
+    ()
+
+
+printfn $"%A{part1}"
+
+let part2 =
+    ()
+
+printfn $"%A{part2}"
